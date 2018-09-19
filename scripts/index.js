efeee6d2241eb686546781c252917aa55f016115
youyueqian <1105552532@qq.com>
2018-09-19 08:13:25 +0800

    third 3

--- scripts/index.js
+++ scripts/index.js
@@ -573,7 +573,7 @@
 			}.bind(this));
 		},
 		formatSrc: function (src) {
-			if(src.indexOf("http:")==-1){
+			if(src.indexOf("http:")==-1 || src.indexOf("https") == -1){
 				return "http:" + src;
 			}else{
 				return src;
