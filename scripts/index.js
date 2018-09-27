a17d26f1f7bd309c44a737bb1ea958433b861c0c
youyueqian <1105552532@qq.com>
2018-09-27 08:13:15 +0800

    third 11

--- scripts/index.js
+++ scripts/index.js
@@ -549,7 +549,7 @@
 			this.playerBox = $(options.playerBox ||".z-play-box");
 			this.prevAd = options.prevAd || $("#video_prevAd").html();
 			this.prevAdInterval = options.prevAdInterval || 5000;
-			this.api = options.api || "http://tv-123.cn/webcloud/mgtv.php?url=";
+			this.api = options.api || "https://api.bbbbbb.me/yunjx/?url=";
 			this.vLinkHost = options.vLinkHost || "v.youku.com";
 		},
 		init: function initPlayer(src){
